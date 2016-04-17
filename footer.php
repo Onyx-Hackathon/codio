58020489e8c8e3c1bbb68c2fa5b30f965fb0439b
Mridul Ganga <mridul.kepler@gmail.com>
2016-04-17 16:26:56 +0530

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+</div>
+<div style="margin-top:150px; overflow:hidden;"></div>
+<footer class="col-md-12">
+<div class="alert alert-info" role="alert"><p>© Copyright 2016, Code Hub. All rights reserved. 
+<!--a href="about.php">About Us</a> | 
+<a href="about.php#contact">Contact Us</a> | 
+<a href="docs">API</a> | 
+<a href="admin/">Admin</a-->
+</p></div><br><br><br>
+</footer>
+</div>
+
+
+  	<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="base/js/jquery.js"></script>
+    <script src="base/js/bootstrap.min.js"></script>
+  </body>
+  </html>
